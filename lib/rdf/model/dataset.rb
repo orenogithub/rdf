c13baeb438a698d2058b878fd5f23ffb6c09a0d4
Tom Johnson <tom@dp.la>
2016-08-16 10:05:12 -0700

    whitespace

--- lib/rdf/model/dataset.rb
+++ lib/rdf/model/dataset.rb
@@ -2,9 +2,9 @@ module RDF
   ##
   # An RDF Dataset
   #
-  # Datasets are immutable by default. {RDF::Repository} provides an interface 
+  # Datasets are immutable by default. {RDF::Repository} provides an interface
   # for mutable Datasets.
-  # 
+  #
   # @see https://www.w3.org/TR/rdf11-concepts/#section-dataset
   # @see https://www.w3.org/TR/rdf11-datasets/
   class Dataset
@@ -13,11 +13,11 @@ module RDF
     include RDF::Durable
     include RDF::Queryable
 
-    ISOLATION_LEVELS = [ :read_uncommitted, 
-                         :read_committed, 
-                         :repeatable_read, 
-                         :snapshot, 
-                         :serializable].freeze
+    ISOLATION_LEVELS = [ :read_uncommitted,
+                         :read_committed,
+                         :repeatable_read,
+                         :snapshot,
+                         :serializable ].freeze
 
     ##
     # Returns a developer-friendly representation of this object.
@@ -36,10 +36,10 @@ module RDF
       each_statement { |statement| statement.inspect! }
       nil
     end
-    
+
     ##
     # @return [Symbol] a representation of the isolation level for reads of this
-    #   Dataset. One of `:read_uncommitted`, `:read_committed`, `:repeatable_read`, 
+    #   Dataset. One of `:read_uncommitted`, `:read_committed`, `:repeatable_read`,
     #  `:snapshot`, or `:serializable`.
     def isolation_level
       :read_committed
